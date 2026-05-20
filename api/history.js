3d29dae45fb89f6e260e3746744dfcc3e05a82b4
AliSindhibwia82b <sajanbhai261@gmail.com>
2026-05-20 16:47:16 +0500

    history.js

--- api/history.js
+++ api/history.js
@@ -1,16 +1,31 @@
 export default async function handler(req, res) {
 
+try {
+
 const response = await fetch(
 'https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json'
 );
 
-const data = await response.json();
+const text = await response.text();
 
 res.setHeader(
 'Access-Control-Allow-Origin',
 '*'
 );
 
-res.status(200).json(data);
+res.setHeader(
+'Content-Type',
+'application/json'
+);
+
+res.status(200).send(text);
+
+} catch (e) {
+
+res.status(500).json({
+error: e.toString()
+});
+
+}
 
 }
