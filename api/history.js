0e36fd7297981075b4c3f916a0139ff02195e26d
AliSindhibwia82b <sajanbhai261@gmail.com>
2026-05-20 16:57:54 +0500

    history.js

--- api/history.js
+++ api/history.js
@@ -3,7 +3,13 @@ export default async function handler(req, res) {
 try {
 
 const response = await fetch(
-'https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json'
+'https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json',
+{
+headers: {
+'User-Agent':
+'Mozilla/5.0'
+}
+}
 );
 
 const text = await response.text();
